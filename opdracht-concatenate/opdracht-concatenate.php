ef5b1792d228124cae0eacaaadb2a31cf16c5404
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2016-10-07 19:14:05 +0200

    Toevoegen map opdracht concatenate + maken basisch structuur html, php.

---
+++ opdracht-concatenate/opdracht-concatenate.php
@@ -0,0 +1,31 @@
+<?php
+
+    
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/directory.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/facade.css">
+</head>
+
+<body class="web-backend-inleiding">
+
+	<section class="body">
+
+		<h1>Contatenate opdracht:</h1>
+		
+		
+
+	</section>
+
+</body>
+</html>

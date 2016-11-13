577e5544a3e024e559812ab6a91c84d8423a3ea0
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2016-11-13 17:12:29 +0100

    Toevoegen opdracht include/require.

---
+++ opdracht-include-require/index.php
@@ -0,0 +1,28 @@
+<?php
+
+  $artikels       =   array(  array("title" =>  "Clinton geeft FBI-directeur Comey de schuld van haar verlies",
+                                    "text"  =>  "De Amerikaanse presidentskandidaat Hillary Clinton heeft de schuld van haar nederlaag in de presidentsverkiezingen gelegd bij FBI-directeur James B. Comey. Doordat hij een nieuw onderzoek naar het e-mailgebruik van Clinton heeft aangekondigd, zou hij de schijn van partijdigheid hebben gewekt.",
+                                    "tags"  =>  array("Hillary Clinton",
+                                                      "Verkiezingen VS",
+                                                      "Verenigde Staten van Amerika")
+                                  ),
+
+                              array("title" =>  "'Ik hoef niet één cent': laat Trump het presidentieel loon van 400.000 dollar liggen?",
+                                    "text"  =>  "Nu Donald Trump het pleit gewonnen heeft, rijzen er enkele vragen in verband met zijn inkomen. Zal hij het jaarlijkse presidentiële loon van 400.000 dollar (368.000 euro) aanvaarden bijvoorbeeld? Tijdens een campagnebijeenkomst vorig jaar in Rochester (New Hampshire) gaf hij daar nog een duidelijk antwoord op. 'Dat geld wil ik niet, voor mij is dat niet zo belangrijk. Dat zal mijn eerste wapenfeit als president worden.'",
+                                    "tags"  =>  array("Donald Trump",
+                                                      "New York State")
+                                  ),
+
+                              array("title" =>  "Titel artikel 3",
+                                    "text"  =>  "Tekst artikel 3",
+                                    "tags"  =>  array("tag 1 van artikel 3",
+                                                      "tag 2 van artikel 3",
+                                                      "tag 3 van artikel 3")
+                                  )
+                          );
+
+include 'view/header-partial.html';
+include 'view/body-partial.html';
+include 'view/footer-partial.html';
+
+?>

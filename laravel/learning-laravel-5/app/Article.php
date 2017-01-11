0af78725f6574395b5807e4be2f3f121735346be
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2017-01-11 14:31:53 +0100

    Migrations Comments, Articles, Users, Eloquent voor vullen aantal rows van die tabellen.

---
+++ laravel/learning-laravel-5/app/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+
+    //deze mogen geMass assigned worden
+    protected $fillable = [
+
+        'title',
+        'url',
+        'text',
+        'published_at'
+
+    ];
+
+}
